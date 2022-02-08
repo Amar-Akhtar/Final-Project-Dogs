582a32f65b1eb3c10519562e27d51b979fb7c880
lifebeginsat30 <amar.akhtar@icloud.com>
2022-02-08 09:58:17 +0000

    Added getters and setters

--- src/main/java/com/qa/dogs/domain/Dogs.java
+++ src/main/java/com/qa/dogs/domain/Dogs.java
@@ -46,4 +46,31 @@ public class Dogs {
 		this.name = name;
 		this.age = age;
 	}
+
+	// Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
