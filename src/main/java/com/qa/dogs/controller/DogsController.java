c0e3ee766f83166e6ac62874c5077c3ca17de8f8
lifebeginsat30 <amar.akhtar@icloud.com>
2022-02-08 10:26:19 +0000

    Read method complete

--- src/main/java/com/qa/dogs/controller/DogsController.java
+++ src/main/java/com/qa/dogs/controller/DogsController.java
@@ -1,7 +1,10 @@
 package com.qa.dogs.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,12 @@ public class DogsController {
 	@PostMapping("/createDog")
 	public ResponseEntity<Dogs> createDog(@RequestBody Dogs a){
 		return new ResponseEntity<Dogs>(this.dogService.create(a), HttpStatus.CREATED);
+	}
+	
+	//Read
+	@GetMapping("/getDog")
+	public ResponseEntity<List<Dogs>> getDog(){
+		return new ResponseEntity<List<Dogs>>(this.dogService.getAll(), HttpStatus.OK);
+		
 	}
 }
