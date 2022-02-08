2222ae5669c56a322f4320937ec18b8c41b74bc5
lifebeginsat30 <amar.akhtar@icloud.com>
2022-02-08 10:28:39 +0000

    Read-By-Id method complete

--- src/main/java/com/qa/dogs/controller/DogsController.java
+++ src/main/java/com/qa/dogs/controller/DogsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,13 @@ public class DogsController {
 	@GetMapping("/getDog")
 	public ResponseEntity<List<Dogs>> getDog(){
 		return new ResponseEntity<List<Dogs>>(this.dogService.getAll(), HttpStatus.OK);
+		
+	}
+	
+	//Read-by-Id
+	@GetMapping("/getId")
+	public ResponseEntity<Dogs> getId(@PathVariable Long id){
+		return new ResponseEntity<Dogs>(this.dogService.getById(id), HttpStatus.FOUND);
 		
 	}
 }
