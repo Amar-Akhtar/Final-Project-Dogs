a0f8fefb2379db046745b07a21dc26511e54a281
lifebeginsat30 <amar.akhtar@icloud.com>
2022-02-08 09:47:22 +0000

    initial commit

---
+++ src/main/java/com/qa/dogs/FinalProjectDfesw8Application.java
@@ -0,0 +1,13 @@
+package com.qa.dogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectDfesw8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectDfesw8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dogs/FinalProjectDfesw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectDfesw8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
