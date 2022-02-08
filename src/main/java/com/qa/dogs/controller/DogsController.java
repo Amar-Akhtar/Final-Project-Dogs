a46a555db5b5d6339c994d67c7d66893b91370c5
lifebeginsat30 <amar.akhtar@icloud.com>
2022-02-08 10:30:51 +0000

    Update method complete

--- src/main/java/com/qa/dogs/controller/DogsController.java
+++ src/main/java/com/qa/dogs/controller/DogsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,11 @@ public class DogsController {
 	public ResponseEntity<Dogs> getId(@PathVariable Long id){
 		return new ResponseEntity<Dogs>(this.dogService.getById(id), HttpStatus.FOUND);
 		
+	}
+	
+	//update
+	@PutMapping("/updatedog/{id}")
+	public ResponseEntity<Dogs> updateDog(@PathVariable Long id, @RequestBody Dogs b){
+		return new ResponseEntity<Dogs>(this.dogService.update(id, b), HttpStatus.OK);
 	}
 }
